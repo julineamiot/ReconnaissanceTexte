940f006b28534236355a175cb65965968cd87b15
elievuillot-creator <elie.vuillot@gmail.com>
2026-01-22 10:28:57 +0100

    ClasseFictive22/01/25

---
+++ CFF.py
@@ -0,0 +1,23 @@
+class NeuronalNetwork:
+    def __init__(self):
+        result = []
+        return None
+
+    # partie pré-gestion des données
+
+    def OpenIMGtxt(self):
+        return None # matrice de pixel
+
+    def DecImg(self):
+        return None # matrice de matrice de mm taille
+
+    def Analyse(self):
+        for i in self.DecImg():
+            for j in self.DecImg():
+                # calc les probas pour tt les chiffres avec notre rés de neuronnes initiales
+                # garder la plus grande proba
+                value = value
+                self.result.append(value)
+        return None
+
+
